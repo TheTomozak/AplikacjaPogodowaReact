3995519961422094deb7cdef46e4e02d1ac136f5
TheTomozak <tomaszpalczewski99@gmail.com>
2020-08-24 01:27:14 +0200

    Dodano metode fetch

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -5,23 +5,75 @@ import Result from "./Result";
 
 class App extends Component {
 
-    state ={
-      value : ''
 
+    state = {
+        value: '',
+        date: '',
+        cityName: '',
+        sunrise: '',
+        sunset: '',
+        temp: '',
+        pressure: '',
+        wind: '',
+        err: ''
     };
 
-    handleInputChange =(event) => {
+
+    handleInputChange = (event) => {
         this.setState({
-            value :event.target.value
+            value: event.target.value
         })
     };
+
+    handleCitySubmit = (event) => {
+        event.preventDefault();
+
+        const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=b80af7ab94eb83330497a9ffbd5ff6fe&units=metric`;
+        fetch(API)
+            .then(response =>{
+               if(response.ok){
+                   return response
+               }
+               throw Error('Nie udało się')
+            }).then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.warn(err))
+        
+        // this.componentDidMount()
+
+
+    };
+
+
+    // async componentDidMount() {
+    //     const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=b80af7ab94eb83330497a9ffbd5ff6fe&units=metric`;
+    //     let response;
+    //     try {
+    //          response = await fetch(API);
+    //     } catch (e) {
+    //         console.warn(e);
+    //         throw Error('Nie udało się pobrać API')
+    //     }
+    //
+    //         const isOk = response.ok;
+    //         if (!isOk) {
+    //             return Promise.reject("Nie udało się")
+    //         }
+    //         const data = await response.json();
+    //         return console.log(data);
+    //
+    // }
 
 
     render() {
         return (
             <div className="App">
-                <Form value = {this.state.value} change={this.handleInputChange}/>
-                <Result />
+                <Form
+                    value={this.state.value}
+                    change={this.handleInputChange}
+                    submit={this.handleCitySubmit}
+                />
+                <Result/>
             </div>
         );
     }
