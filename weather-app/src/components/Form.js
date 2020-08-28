e121ee88b54818a474329d460ff7b14d68dc5cca
s18969 <tomaszpalczewski99@gmail.com>
2020-08-28 16:07:02 +0200

    Dodano wyświetlanie pełnych nazw państw oraz usunieto ramkę kiedy flaga jest nie załadowana

--- weather-app/src/components/Form.js
+++ weather-app/src/components/Form.js
@@ -1,17 +1,27 @@
 import React from "react";
 
 const Form = (props) =>{
+
+    const countries = require("i18n-iso-countries");
+    countries.registerLocale(require("i18n-iso-countries/langs/pl.json"));
+    const country = countries.getName(props.countryShort, "pl");
+    let counryAndShortcut = country + " (" + props.countryShort+")";
+    if(props.countryShort.length ===0){
+        counryAndShortcut='';
+    }
+    
+
     return(
         <>
         <form className={"Form"} >
-            {props.countryShort}
+            {counryAndShortcut}
             <input
                 type={"text"}
                 value={props.value}
                 placeholder={"Wpisz miasto"}
                 onChange={props.change}
             />
-            <img alt={null} className={"Flaga"} src={props.countryShort ? `https://www.countryflags.io/${props.countryShort}/shiny/64.png` : "" } />
+            <img alt={"flaga"} className={"Flaga"} src={props.countryShort ? `https://www.countryflags.io/${props.countryShort}/shiny/64.png` : "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" } />
         </form>
 
         </>
